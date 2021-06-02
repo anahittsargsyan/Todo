1957d0f04a4c07a06292b2295a7e6fb68a4cfe1e
Anan <anahit@Anahits-MacBook-Air.local>
2021-06-02 18:43:27 +0400

    code changed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 
-
 interface Todo {
   done: boolean;
   description: string;
@@ -9,10 +8,9 @@ interface Todo {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
   done = null;
   count = null;
   title = 'v-todolist';
@@ -21,20 +19,21 @@ export class AppComponent {
   activeFilter = 'all';
 
   get displayedTodos() {
-    this.count = this.todos.filter(todo => !todo.done).map(todo => todo.description).length;
-    switch(this.activeFilter) {
+    this.count = this.todos.filter((todo) => !todo.done).length;
+    switch (this.activeFilter) {
       case 'all':
         return this.todos;
       case 'done':
-        return this.todos.filter(todo => todo.done);
+        return this.todos.filter((todo) => todo.done);
       case 'incomplete':
-        return this.todos.filter(todo => !todo.done);
+        return this.todos.filter((todo) => !todo.done);
     }
-
   }
+
   ngOnInit(): void {
     this.todos = this.getItemsFromStorage();
   }
+
   addTodo() {
     if (!this.todoDescription) {
       return alert('description required');
@@ -42,42 +41,40 @@ export class AppComponent {
 
     this.todos.push({
       done: false,
-      description: this.todoDescription
+      description: this.todoDescription,
     });
 
     this.todoDescription = '';
     this.saveItemsToStorage();
-    this.count = this.todos.filter(todo => !todo.done).map(todo => todo.description).length;
   }
 
   clearCompleted() {
-    this.todos =this.todos.filter(todo => !todo.done);
+    this.todos = this.todos.filter((todo) => !todo.done);
   }
 
   removeTodo(i) {
-    this.todos.splice(i,1)
+    this.todos.splice(i, 1);
   }
 
   selectAll() {
     let isCheck = false;
-    if(this.todos.filter(x => x.done).map(x => x.description).length != this.todos.length) {
-      isCheck = true
+    if (this.todos.filter((x) => x.done).length != this.todos.length) {
+      isCheck = true;
     }
     this.todos.forEach((item) => {
-       item.done = isCheck;
-      }
-    );
-    }
-  saveItemsToStorage(){
-      localStorage.setItem('items', JSON.stringify(this.todos));
+      item.done = isCheck;
+    });
+  }
+  saveItemsToStorage() {
+    localStorage.setItem('items', JSON.stringify(this.todos));
   }
 
   getItemsFromStorage() {
-      const itemsJson = localStorage.getItem('items');
-      if (itemsJson) {
-        const items = JSON.parse(itemsJson);
-        return items;
-      }
-      return [];
+    const itemsJson = localStorage.getItem('items');
+    if (itemsJson) {
+      const items = JSON.parse(itemsJson);
+      return items;
+    }
+    return [];
   }
 }
